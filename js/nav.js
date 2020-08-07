9251c06056bcf33d9026ae6d3ce043643df25e4c
‘Ama_lion’ <bahamadu2019@gmail.com>
2020-08-07 09:47:19 +0200

    project finished

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+const scrolToTop = document.querySelector("#scrolTOTop");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   links.forEach((link) => {
     link.classList.toggle("fade");
   });
+});
+
+scrolToTop.addEventListener("click", function () {
+  window.scrollTo(0, 0);
 });
